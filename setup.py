9e8e92c122bb7c1431b101779ce3846dfab45ed5
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-04 12:22:05 +0300

    prepare for version 0.2 release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="seriesly",
-    version="0.1",
+    version="0.2",
     description="Python client for seriesly database.",
     author="Couchbase, Inc.",
     author_email="pavel.paulau@couchbase.com",
